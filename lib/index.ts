26ee759492d72594ddf29cc6d7e99885b45688bf
Igor N. Dultsev <yhaskell@live.com>
2017-06-26 17:33:58 +0700

    resolve components inside middleware

--- lib/index.ts
+++ lib/index.ts
@@ -1,17 +1,24 @@
 import * as express from 'express'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/server'
+import * as path from 'path'
 
 import * as watcher from './watch-for-root-component'
 
-import webpackDevHmrMiddlewareFactory from './webpack-dev-hmr'
+import webpackDevHmrMiddlewareFactory from './webpack-dev-middleware'
 import middlewareFactory from './middleware-factory'
 import { defaultRenderer, JSONData, Renderer } from './html-renderer'
 import { InternalOptions, Options } from './options'
 
-
 function factory(rootComponentPath: string, opts?: Partial<Options>) {
     opts = opts || {}
+    if (module.parent) { // actually this will always be true
+        const fromDirname = path.dirname(module.parent.filename)
+
+        if (!path.isAbsolute(rootComponentPath))
+            rootComponentPath = path.resolve(fromDirname, rootComponentPath)
+        if (opts.withRouter && !path.isAbsolute(opts.withRouter)) opts.withRouter = path.resolve(fromDirname, opts.withRouter)
+    }
     opts.vendorScripts = opts.vendorScripts || []
 
     const env = process.env.NODE_ENV
